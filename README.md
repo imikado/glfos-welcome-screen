3d7d7f3d114fc3f736fb5cd663763ffa3c876e64
imikado <imikado@gmail.com>
2025-05-24 11:33:50 +0200

    update usefull links

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # glfos-welcome-screen
 Welcome screen for glf os (a linux distribution, based on nixos)
+
+## Build the derivation and test it
+
+```bash
+git pull https://github.com/imikado/glfos-welcome-screen.git
+./buildNixDerivation.sh
+sudo ./result/bin/nix_disk_manager
+```
--- lib/View/usefulinformation_view.dart
+++ lib/View/usefulinformation_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:glfos_welcome_screen/Api/localization_api.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class UsefulinformationView extends StatelessWidget {
+class UsefulinformationView extends StatefulWidget {
   const UsefulinformationView({
     super.key,
   });
+
+  @override
+  State<UsefulinformationView> createState() => _UsefulinformationViewState();
+}
+
+class _UsefulinformationViewState extends State<UsefulinformationView> {
+  List<LinkItem> links = [
+    LinkItem(label: 'GLF OS', url: 'https://www.gaminglinux.fr/glf-os/'),
+    LinkItem(label: 'Wiki', url: 'https://gaming-linux-fr.github.io/GLF-OS/'),
+    LinkItem(label: 'Discord', url: 'https://discord.gg/tqXyUMEwq3'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +32,31 @@ class UsefulinformationView extends StatelessWidget {
           SizedBox(
             height: 15,
           ),
-          Text(LocalizationApi().tr('page_usefulinformation_body'))
+          Text(LocalizationApi().tr('page_usefulinformation_body')),
+          SizedBox(
+            height: 15,
+          ),
+          Column(
+            children: links
+                .map((LinkItem linkLoop) => Padding(
+                    padding: EdgeInsets.all(3),
+                    child: TextButton.icon(
+                        icon: Icon(Icons.link),
+                        onPressed: () {
+                          launchUrl(Uri.parse(linkLoop.url));
+                        },
+                        label: Text(linkLoop.label))))
+                .toList(),
+          )
         ],
       ),
     ]));
   }
+}
+
+class LinkItem {
+  final String label;
+  final String url;
+
+  LinkItem({required this.label, required this.url});
 }
