482e66f749fb0e6d4386d90a8c15d458582ea787
imikado <imikado@gmail.com>
2025-06-12 13:26:47 +0200

    upd

--- buildAppImage.sh
+++ buildAppImage.sh
@@ -2,7 +2,7 @@ rm -rf /tmp/glfosWelcomeScreen.AppDir
 flutter build linux --release
 cp -r build/linux/x64/release/bundle/ /tmp/glfosWelcomeScreen.AppDir
 cp assets/logo.png /tmp/glfosWelcomeScreen.AppDir/glfosWelcomeScreen.png
-cp appImage/AppRun /tmp/glfosWelcomeScreen.AppDir/glfosWelcomeScreen.png
+cp appImage/AppRun /tmp/glfosWelcomeScreen.AppDir/AppRun
 chmod +x /tmp/glfosWelcomeScreen.AppDir/AppRun
 cp appImage/glfosWelcomeScreen.desktop /tmp/glfosWelcomeScreen.AppDir/
 appimagetool-x86_64.AppImage /tmp/glfosWelcomeScreen.AppDir
