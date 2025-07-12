2f44b9e78a201afffb3b3f893cb09a295161981e
imikado <imikado@gmail.com>
2025-07-13 01:21:11 +0200

    try launch with gnome terminal

--- lib/View/Shared/sharedmarkdown_view.dart
+++ lib/View/Shared/sharedmarkdown_view.dart
@@ -67,11 +67,8 @@ class _SharedMarkdownViewState extends State<SharedMarkdownView> {
       await Process.run('flatpak', ['run', command]);
       return;
     } else if (commandName.startsWith('bash://')) {
-      await Process.run('env', []).then((result) {
-        print(result.stdout);
-      });
-
-      String command = commandName.replaceAll('bash://', '');
+      String command = commandName.replaceAll('bash://', 'gnome-terminal -- ');
+      print('try to launch "$command"');
       var result = await Process.run(command, []);
       print(result.stdout);
 
