34fa811a7f217b915eb3d33b59ad16744eb68c38
imikado <imikado@gmail.com>
2025-07-28 17:01:21 +0200

    add other format for flake pubspec.nix

---
+++ pubspec.nix
@@ -0,0 +1,575 @@
+packages:
+  adwaita:
+    dependency: direct main
+    description:
+      name: adwaita
+      sha256: 91be996e0e3a88c5cc42251af5559707a712f847995240a27fdf71e1dc44630e
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.0
+    sha256: 1kw84avnwxlzns5c7g3l9dhfrxh6m2j7xyzkiiks83fsrlpkafgk
+  args:
+    dependency: transitive
+    description:
+      name: args
+      sha256: d0481093c50b1da8910eb0bb301626d4d8eb7284aa739614d2b394ee09e3ea04
+      url: https://pub.dev
+    source: hosted
+    version: 2.7.0
+    sha256: 09n96mvcwni9niwqdk4slq2y018wfbrybpyyrk4frw2rw03lxs8x
+  async:
+    dependency: transitive
+    description:
+      name: async
+      sha256: 947bfcf187f74dbc5e146c9eb9c0f10c9f8b30743e341481c1e2ed3ecc18c20c
+      url: https://pub.dev
+    source: hosted
+    version: 2.11.0
+    sha256: 0hfgvjajp5c2mw68186hgrk9v5zjhhi149hlhl0fap274p2v1g3q
+  boolean_selector:
+    dependency: transitive
+    description:
+      name: boolean_selector
+      sha256: 6cfb5af12253eaf2b368f07bacc5a80d1301a071c73360d746b7f2e32d762c66
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.1
+    sha256: 0hxq8072hb89q9s91xlz9fvrjxfy7hw6jkdwkph5dp77df841kmj
+  characters:
+    dependency: transitive
+    description:
+      name: characters
+      sha256: 04a925763edad70e8443c99234dc3328f442e811f1d8fd1a72f1c8ad0f69a605
+      url: https://pub.dev
+    source: hosted
+    version: 1.3.0
+    sha256: 0azl2kw9mv2yqd1xiq54zfykmv0bm9j4aqb3b4rrkf9a9xn29sy7
+  clock:
+    dependency: transitive
+    description:
+      name: clock
+      sha256: cb6d7f03e1de671e34607e909a7213e31d7752be4fb66a86d29fe1eb14bfb5cf
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.1
+    sha256: 1z1mpmd7b6dw9w20vb5l5y3r6fmck4iriydlv9hhvmp7p6ss37hk
+  collection:
+    dependency: transitive
+    description:
+      name: collection
+      sha256: a1ace0a119f20aabc852d165077c036cd864315bd99b7eaa10a60100341941bf
+      url: https://pub.dev
+    source: hosted
+    version: 1.19.0
+    sha256: 1yh9sw50kz1mhk4g0dfwh5gak32ap3hwdrk8lyjbx6galvbi1rnm
+  dbus:
+    dependency: transitive
+    description:
+      name: dbus
+      sha256: 79e0c23480ff85dc68de79e2cd6334add97e48f7f4865d17686dd6ea81a47e8c
+      url: https://pub.dev
+    source: hosted
+    version: 0.7.11
+    sha256: 0inh686vnhj9nnqham0yhrcz4r5q1xmbrdsn7khcpdl1ishdbmsf
+  fake_async:
+    dependency: transitive
+    description:
+      name: fake_async
+      sha256: 511392330127add0b769b75a987850d136345d9227c6b94c96a04cf4a391bf78
+      url: https://pub.dev
+    source: hosted
+    version: 1.3.1
+    sha256: 0xyswdmj7f7ns0qhfq1s8z0wnh0gd277l73az1dh0x1zd6l7wp7k
+  ffi:
+    dependency: transitive
+    description:
+      name: ffi
+      sha256: 16ed7b077ef01ad6170a3d0c57caa4a112a38d7a2ed5602e0aca9ca6f3d98da6
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.3
+    sha256: 19wipld17k3nw7m8a0vxaksvq0mpfldq2y08z3mqivl6x5x0qdq9
+  flutter:
+    dependency: direct main
+    description: flutter
+    source: sdk
+    version: 0.0.0
+  flutter_localizations:
+    dependency: direct main
+    description: flutter
+    source: sdk
+    version: 0.0.0
+  flutter_markdown:
+    dependency: direct main
+    description:
+      name: flutter_markdown
+      sha256: 08fb8315236099ff8e90cb87bb2b935e0a724a3af1623000a9cec930468e0f27
+      url: https://pub.dev
+    source: hosted
+    version: 0.7.7+1
+  flutter_svg:
+    dependency: transitive
+    description:
+      name: flutter_svg
+      sha256: d44bf546b13025ec7353091516f6881f1d4c633993cb109c3916c3a0159dadf1
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.0
+  flutter_test:
+    dependency: direct dev
+    description: flutter
+    source: sdk
+    version: 0.0.0
+  flutter_web_plugins:
+    dependency: transitive
+    description: flutter
+    source: sdk
+    version: 0.0.0
+  gsettings:
+    dependency: transitive
+    description:
+      name: gsettings
+      sha256: 1b0ce661f5436d2db1e51f3c4295a49849f03d304003a7ba177d01e3a858249c
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.8
+  http:
+    dependency: transitive
+    description:
+      name: http
+      sha256: 2c11f3f94c687ee9bad77c171151672986360b2b001d109814ee7140b2cf261b
+      url: https://pub.dev
+    source: hosted
+    version: 1.4.0
+  http_parser:
+    dependency: transitive
+    description:
+      name: http_parser
+      sha256: 178d74305e7866013777bab2c3d8726205dc5a4dd935297175b19a23a2e66571
+      url: https://pub.dev
+    source: hosted
+    version: 4.1.2
+  intl:
+    dependency: transitive
+    description:
+      name: intl
+      sha256: d6f56758b7d3014a48af9701c085700aac781a92a87a62b1333b46d8879661cf
+      url: https://pub.dev
+    source: hosted
+    version: 0.19.0
+  json_annotation:
+    dependency: transitive
+    description:
+      name: json_annotation
+      sha256: 1ce844379ca14835a50d2f019a3099f419082cfdd231cd86a142af94dd5c6bb1
+      url: https://pub.dev
+    source: hosted
+    version: 4.9.0
+  launch_at_startup:
+    dependency: direct main
+    description:
+      name: launch_at_startup
+      sha256: 1f8a75520913d1038630049e6c44a2575a23ffd28cc8b14fdf37401d1d21de84
+      url: https://pub.dev
+    source: hosted
+    version: 0.3.1
+  leak_tracker:
+    dependency: transitive
+    description:
+      name: leak_tracker
+      sha256: 7bb2830ebd849694d1ec25bf1f44582d6ac531a57a365a803a6034ff751d2d06
+      url: https://pub.dev
+    source: hosted
+    version: 10.0.7
+  leak_tracker_flutter_testing:
+    dependency: transitive
+    description:
+      name: leak_tracker_flutter_testing
+      sha256: 9491a714cca3667b60b5c420da8217e6de0d1ba7a5ec322fab01758f6998f379
+      url: https://pub.dev
+    source: hosted
+    version: 3.0.8
+  leak_tracker_testing:
+    dependency: transitive
+    description:
+      name: leak_tracker_testing
+      sha256: 6ba465d5d76e67ddf503e1161d1f4a6bc42306f9d66ca1e8f079a47290fb06d3
+      url: https://pub.dev
+    source: hosted
+    version: 3.0.1
+  libadwaita:
+    dependency: direct main
+    description:
+      name: libadwaita
+      sha256: 1c1079587b21018e71173f0a0682797c255d166ef79fd470c636eedbd69f7fa3
+      url: https://pub.dev
+    source: hosted
+    version: 2.0.2
+  libadwaita_core:
+    dependency: transitive
+    description:
+      name: libadwaita_core
+      sha256: 1a7689e4130e96be4b0ec615863fded25cd86c2b95a682e7cec9659d420de37c
+      url: https://pub.dev
+    source: hosted
+    version: 0.5.4
+  libadwaita_window_manager:
+    dependency: direct main
+    description:
+      name: libadwaita_window_manager
+      sha256: 196b705b9683d2979740e9bfd46bbe1ce69fc16af217215d80df7c84ff8fe30f
+      url: https://pub.dev
+    source: hosted
+    version: 0.5.4+1
+  logging:
+    dependency: direct main
+    description:
+      name: logging
+      sha256: c8245ada5f1717ed44271ed1c26b8ce85ca3228fd2ffdb75468ab01979309d61
+      url: https://pub.dev
+    source: hosted
+    version: 1.3.0
+  markdown:
+    dependency: transitive
+    description:
+      name: markdown
+      sha256: 935e23e1ff3bc02d390bad4d4be001208ee92cc217cb5b5a6c19bc14aaa318c1
+      url: https://pub.dev
+    source: hosted
+    version: 7.3.0
+  matcher:
+    dependency: transitive
+    description:
+      name: matcher
+      sha256: d2323aa2060500f906aa31a895b4030b6da3ebdcc5619d14ce1aada65cd161cb
+      url: https://pub.dev
+    source: hosted
+    version: 0.12.16+1
+  material_color_utilities:
+    dependency: transitive
+    description:
+      name: material_color_utilities
+      sha256: f7142bb1154231d7ea5f96bc7bde4bda2a0945d2806bb11670e30b850d56bdec
+      url: https://pub.dev
+    source: hosted
+    version: 0.11.1
+  meta:
+    dependency: transitive
+    description:
+      name: meta
+      sha256: bdb68674043280c3428e9ec998512fb681678676b3c54e773629ffe74419f8c7
+      url: https://pub.dev
+    source: hosted
+    version: 1.15.0
+  package_info_plus:
+    dependency: direct main
+    description:
+      name: package_info_plus
+      sha256: 7e76fad405b3e4016cd39d08f455a4eb5199723cf594cd1b8916d47140d93017
+      url: https://pub.dev
+    source: hosted
+    version: 4.2.0
+  package_info_plus_platform_interface:
+    dependency: transitive
+    description:
+      name: package_info_plus_platform_interface
+      sha256: 9bc8ba46813a4cc42c66ab781470711781940780fd8beddd0c3da62506d3a6c6
+      url: https://pub.dev
+    source: hosted
+    version: 2.0.1
+  path:
+    dependency: transitive
+    description:
+      name: path
+      sha256: 087ce49c3f0dc39180befefc60fdb4acd8f8620e5682fe2476afd0b3688bb4af
+      url: https://pub.dev
+    source: hosted
+    version: 1.9.0
+  path_parsing:
+    dependency: transitive
+    description:
+      name: path_parsing
+      sha256: 883402936929eac138ee0a45da5b0f2c80f89913e6dc3bf77eb65b84b409c6ca
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.0
+  petitparser:
+    dependency: transitive
+    description:
+      name: petitparser
+      sha256: c15605cd28af66339f8eb6fbe0e541bfe2d1b72d5825efc6598f3e0a31b9ad27
+      url: https://pub.dev
+    source: hosted
+    version: 6.0.2
+  plugin_platform_interface:
+    dependency: transitive
+    description:
+      name: plugin_platform_interface
+      sha256: 4820fbfdb9478b1ebae27888254d445073732dae3d6ea81f0b7e06d5dedc3f02
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.8
+  popover_gtk:
+    dependency: transitive
+    description:
+      name: popover_gtk
+      sha256: c293bfa3bcb9436fe189f20f6397199ce996ba40d4b081e26793adcf3ceac45b
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.6+3
+  screen_retriever:
+    dependency: transitive
+    description:
+      name: screen_retriever
+      sha256: 570dbc8e4f70bac451e0efc9c9bb19fa2d6799a11e6ef04f946d7886d2e23d0c
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.0
+  screen_retriever_linux:
+    dependency: transitive
+    description:
+      name: screen_retriever_linux
+      sha256: f7f8120c92ef0784e58491ab664d01efda79a922b025ff286e29aa123ea3dd18
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.0
+  screen_retriever_macos:
+    dependency: transitive
+    description:
+      name: screen_retriever_macos
+      sha256: 71f956e65c97315dd661d71f828708bd97b6d358e776f1a30d5aa7d22d78a149
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.0
+  screen_retriever_platform_interface:
+    dependency: transitive
+    description:
+      name: screen_retriever_platform_interface
+      sha256: ee197f4581ff0d5608587819af40490748e1e39e648d7680ecf95c05197240c0
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.0
+  screen_retriever_windows:
+    dependency: transitive
+    description:
+      name: screen_retriever_windows
+      sha256: 449ee257f03ca98a57288ee526a301a430a344a161f9202b4fcc38576716fe13
+      url: https://pub.dev
+    source: hosted
+    version: 0.2.0
+  sky_engine:
+    dependency: transitive
+    description: flutter
+    source: sdk
+    version: 0.0.0
+  source_span:
+    dependency: transitive
+    description:
+      name: source_span
+      sha256: 53e943d4206a5e30df338fd4c6e7a077e02254531b138a15aec3bd143c1a8b3c
+      url: https://pub.dev
+    source: hosted
+    version: 1.10.0
+  stack_trace:
+    dependency: transitive
+    description:
+      name: stack_trace
+      sha256: 9f47fd3630d76be3ab26f0ee06d213679aa425996925ff3feffdec504931c377
+      url: https://pub.dev
+    source: hosted
+    version: 1.12.0
+  stream_channel:
+    dependency: transitive
+    description:
+      name: stream_channel
+      sha256: ba2aa5d8cc609d96bbb2899c28934f9e1af5cddbd60a827822ea467161eb54e7
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.2
+  string_scanner:
+    dependency: transitive
+    description:
+      name: string_scanner
+      sha256: 688af5ed3402a4bde5b3a6c15fd768dbf2621a614950b17f04626c431ab3c4c3
+      url: https://pub.dev
+    source: hosted
+    version: 1.3.0
+  term_glyph:
+    dependency: transitive
+    description:
+      name: term_glyph
+      sha256: a29248a84fbb7c79282b40b8c72a1209db169a2e0542bce341da992fe1bc7e84
+      url: https://pub.dev
+    source: hosted
+    version: 1.2.1
+  test_api:
+    dependency: transitive
+    description:
+      name: test_api
+      sha256: 664d3a9a64782fcdeb83ce9c6b39e78fd2971d4e37827b9b06c3aa1edc5e760c
+      url: https://pub.dev
+    source: hosted
+    version: 0.7.3
+  typed_data:
+    dependency: transitive
+    description:
+      name: typed_data
+      sha256: f9049c039ebfeb4cf7a7104a675823cd72dba8297f264b6637062516699fa006
+      url: https://pub.dev
+    source: hosted
+    version: 1.4.0
+  url_launcher:
+    dependency: direct main
+    description:
+      name: url_launcher
+      sha256: 9d06212b1362abc2f0f0d78e6f09f726608c74e3b9462e8368bb03314aa8d603
+      url: https://pub.dev
+    source: hosted
+    version: 6.3.1
+  url_launcher_android:
+    dependency: transitive
+    description:
+      name: url_launcher_android
+      sha256: 8582d7f6fe14d2652b4c45c9b6c14c0b678c2af2d083a11b604caeba51930d79
+      url: https://pub.dev
+    source: hosted
+    version: 6.3.16
+  url_launcher_ios:
+    dependency: transitive
+    description:
+      name: url_launcher_ios
+      sha256: 7f2022359d4c099eea7df3fdf739f7d3d3b9faf3166fb1dd390775176e0b76cb
+      url: https://pub.dev
+    source: hosted
+    version: 6.3.3
+  url_launcher_linux:
+    dependency: transitive
+    description:
+      name: url_launcher_linux
+      sha256: 4e9ba368772369e3e08f231d2301b4ef72b9ff87c31192ef471b380ef29a4935
+      url: https://pub.dev
+    source: hosted
+    version: 3.2.1
+  url_launcher_macos:
+    dependency: transitive
+    description:
+      name: url_launcher_macos
+      sha256: 17ba2000b847f334f16626a574c702b196723af2a289e7a93ffcb79acff855c2
+      url: https://pub.dev
+    source: hosted
+    version: 3.2.2
+  url_launcher_platform_interface:
+    dependency: transitive
+    description:
+      name: url_launcher_platform_interface
+      sha256: 552f8a1e663569be95a8190206a38187b531910283c3e982193e4f2733f01029
+      url: https://pub.dev
+    source: hosted
+    version: 2.3.2
+  url_launcher_web:
+    dependency: transitive
+    description:
+      name: url_launcher_web
+      sha256: 4bd2b7b4dc4d4d0b94e5babfffbca8eac1a126c7f3d6ecbc1a11013faa3abba2
+      url: https://pub.dev
+    source: hosted
+    version: 2.4.1
+  url_launcher_windows:
+    dependency: transitive
+    description:
+      name: url_launcher_windows
+      sha256: 3284b6d2ac454cf34f114e1d3319866fdd1e19cdc329999057e44ffe936cfa77
+      url: https://pub.dev
+    source: hosted
+    version: 3.1.4
+  vector_graphics:
+    dependency: transitive
+    description:
+      name: vector_graphics
+      sha256: a4f059dc26fc8295b5921376600a194c4ec7d55e72f2fe4c7d2831e103d461e6
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.19
+  vector_graphics_codec:
+    dependency: transitive
+    description:
+      name: vector_graphics_codec
+      sha256: 99fd9fbd34d9f9a32efd7b6a6aae14125d8237b10403b422a6a6dfeac2806146
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.13
+  vector_graphics_compiler:
+    dependency: transitive
+    description:
+      name: vector_graphics_compiler
+      sha256: 557a315b7d2a6dbb0aaaff84d857967ce6bdc96a63dc6ee2a57ce5a6ee5d3331
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.17
+  vector_math:
+    dependency: transitive
+    description:
+      name: vector_math
+      sha256: 80b3257d1492ce4d091729e3a67a60407d227c27241d6927be0130c98e741803
+      url: https://pub.dev
+    source: hosted
+    version: 2.1.4
+  vm_service:
+    dependency: transitive
+    description:
+      name: vm_service
+      sha256: f6be3ed8bd01289b34d679c2b62226f63c0e69f9fd2e50a6b3c1c729a961041b
+      url: https://pub.dev
+    source: hosted
+    version: 14.3.0
+  web:
+    dependency: transitive
+    description:
+      name: web
+      sha256: 868d88a33d8a87b18ffc05f9f030ba328ffefba92d6c127917a2ba740f9cfe4a
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.1
+  win32:
+    dependency: transitive
+    description:
+      name: win32
+      sha256: daf97c9d80197ed7b619040e86c8ab9a9dad285e7671ee7390f9180cc828a51e
+      url: https://pub.dev
+    source: hosted
+    version: 5.10.1
+  win32_registry:
+    dependency: transitive
+    description:
+      name: win32_registry
+      sha256: 21ec76dfc731550fd3e2ce7a33a9ea90b828fdf19a5c3bcf556fa992cfa99852
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.5
+  window_manager:
+    dependency: direct main
+    description:
+      name: window_manager
+      sha256: 51d50168ab267d344b975b15390426b1243600d436770d3f13de67e55b05ec16
+      url: https://pub.dev
+    source: hosted
+    version: 0.5.0
+  xdg_directories:
+    dependency: transitive
+    description:
+      name: xdg_directories
+      sha256: 7a3f37b05d989967cdddcbb571f1ea834867ae2faa29725fd085180e0883aa15
+      url: https://pub.dev
+    source: hosted
+    version: 1.1.0
+  xml:
+    dependency: transitive
+    description:
+      name: xml
+      sha256: b015a8ad1c488f66851d762d3090a21c600e479dc75e68328c52774040cf9226
+      url: https://pub.dev
+    source: hosted
+    version: 6.5.0
+sdks:
+  dart: '>=3.6.0 <4.0.0'
+  flutter: '>=3.27.0'
